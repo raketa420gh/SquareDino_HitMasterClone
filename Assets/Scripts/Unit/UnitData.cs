5f12f579803d1d5bbfbf682b9857bb24e76e68e5
Andrei Raketa <raketa420311@gmail.com>
2023-08-18 13:19:36 +0300

    scriptable objects fix

--- Assets/Scripts/Unit/UnitData.cs
+++ Assets/Scripts/Unit/UnitData.cs
@@ -3,7 +3,11 @@ using UnityEngine;
 [CreateAssetMenu(order = 51, menuName = "Unit", fileName = "NewUnitData")]
 public class UnitData : ScriptableObject
 {
-    public int MaxHealth = 100;
-    public float MoveSpeed = 5;
-    public Color Color = Color.white;
+    [SerializeField] private int _maxHealth = 100;
+    [SerializeField] private float _moveSpeed = 5;
+    [SerializeField] private Color _color = Color.white;
+
+    public int MaxHealth => _maxHealth;
+    public float MoveSpeed => _moveSpeed;
+    public Color Color => _color;
 }
